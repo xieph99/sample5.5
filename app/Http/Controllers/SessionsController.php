5195de6a75e2283aa8e7fde19e98d2424682ccea
xiepeihui <xiepeihui@dfl.com.cn>
2017-11-13 09:54:23 +0800

    finish user edit, update, index, and destroy actions

--- app/Http/Controllers/SessionsController.php
+++ app/Http/Controllers/SessionsController.php
@@ -7,6 +7,12 @@ use Auth;
 
 class SessionsController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('guest', [
+           'only' => ['create']
+        ]);
+    }
     public function create()
     {
         return view('sessions.create');
@@ -18,12 +24,12 @@ class SessionsController extends Controller
             'email' => 'required|email|max:255',
             'password' => 'required',
         ]);
-        dd($request->all());
+
         if (Auth::attempt($credentials, $request->has('remember'))) {
             // 登录成功后的相关操作
             session()->flash('success', '欢迎回来！');
 
-            return redirect()->route('users.show', [Auth::user()]);
+            return redirect()->intended(route('users.show', [Auth::user()]));
         } else {
             // 登录失败后的相关操作
             session()->flash('dangger', '很抱歉，您的邮箱和密码不匹配！');
